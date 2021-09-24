268a83d92ed089895dc263d5818a5ea27766ab9e
Josephine Chong <josephine.chong@myob.com>
2021-09-24 22:22:10 +1000

    Added conditons to cover the other columns for winning combination in Game evaluator

--- TicTacToe/GameEvaluator.cs
+++ TicTacToe/GameEvaluator.cs
@@ -8,37 +8,38 @@ namespace TicTacToe
         public string GameOutcome(Board board) //could output enum
         {
             var cells = board.Cells;
-            var occurance = 0;
+            var occurrence = 0;
             var outcome = "";
             var win = false;
-            //while loop - while(condition)
-            //for loop - for(initial_state;condition;change_state)
+            //"in progress" (if there is no win and there are empty cells)
+            
             for (int y = 0; y < cells.Length && !win; y++) //down
             {
-                for (int x = 0; x < cells[y].Length; x++) //across //look at time complexity
+                for (int x = 0; x < cells[y].Length; x++) //across 
                 {
                     if (cells[y][x].Contains("X")) 
                     {
-                        occurance += 1;
+                        occurrence += 1;
+                    }
+
+                    if (y == 0 && (x == 0 || x == 1 || x == 2))
+                    {
+                        if (cells[y][x] == cells[y + 1][x] && cells[y + 1][x] == cells[y + 2][x] && cells[y][x] == "X")
+                        {
+                            outcome = "X-Player wins";
+                            win = true; 
+                        }
                     }
                 }
-                if (occurance == 3)
+                if (occurrence == 3)
                 {
                     outcome = "X-Player wins";
                     win = true;
                 }
-                occurance = 0;
+                occurrence = 0;
             }
-            
+
             Console.WriteLine(outcome);
-
-            //"in progress" (if there is no win and there are empty cells)
-            
-            //win combos:
-            //all on the same line (same array),
-            //same vertical (same position in differnt lines),
-            //diagonal (different lines, different positions)
-
             return outcome;
         }
     }
--- TicTacToe_Test/GameEvaluatorTest.cs
+++ TicTacToe_Test/GameEvaluatorTest.cs
@@ -6,6 +6,28 @@ namespace TicTacToe_Test
     public class GameEvaluatorTest
     {
         [Fact]
+        public void Winner_Should_Be_Printed_If_Winning_Combinations_Reached_case2()
+        {
+            //assign
+            var cells = new string[][]
+            {
+                new []{".", ".", "X"},
+                new []{".", ".", "X"},
+                new []{".", ".", "X"}
+            };
+            var board = new Board(cells);
+            var evaluator = new GameEvaluator();
+            var expectedOutcome = "X-Player wins";
+
+            //act
+            var actualOutcome = evaluator.GameOutcome(board);
+
+            //assert
+            Assert.Equal(expectedOutcome, actualOutcome);
+
+        }
+        
+        [Fact]
         public void In_Progress_Should_Be_Printed_If_No_Winning_Combinations_case2()
         {
             //assign
