de055b0e32340b0d5239d41a460d69bfadfe2ee3
Josephine Chong <josephine.chong@myob.com>
2021-10-08 14:49:12 +1100

    adding reg ex to validate user input for coordinates

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace TicTacToe
 {
@@ -22,8 +23,37 @@ namespace TicTacToe
         {
             var player = (insignia.Equals(Insignia.X)) ? "Player 1": "Player 2";
             _console.WriteLine($"{player} enter a coord x,y to place your {insignia} or enter 'q' to give up:"); //player 1 = X
-            var coordinate = _console.ReadLine();
+            bool positionIsValid = false;
+            var coordinate = "";
+            while (!positionIsValid)
+            {
+                coordinate = _console.ReadLine();
+                var pattern = new Regex(@"^[012],[012]$");
+                positionIsValid = pattern.IsMatch(coordinate);
+                if (!positionIsValid)
+                {
+                    _console.WriteLine("Please enter a coord with the format x,y. With x and y being a single digit");
+                }
+            }
             return coordinate;
+        }
+
+        private void SetPlayersPosition(Board board)
+        {
+            bool choiceIsValid = false;
+            while (!choiceIsValid)
+            {
+                var input = Coordinates(Insignia);
+                try
+                {
+                    board.SetChoice(input, Insignia);
+                    choiceIsValid = true;
+                }
+                catch (OverridingException ex)
+                {
+                    _console.WriteLine("Try another position. " + ex.Message);
+                }
+            }
         }
 
         private void Play(Board board)
@@ -34,21 +64,7 @@ namespace TicTacToe
             while (!winner)
             {
                 board.BoardStatus();
-                bool choiceIsValid = false;
-                while (!choiceIsValid)
-                {
-                    var input = Coordinates(Insignia);
-                    try
-                    {
-                        board.SetChoice(input, Insignia);
-                        choiceIsValid = true;
-                    }
-                    catch (OverridingException ex)
-                    {
-                        _console.WriteLine("Try another position. " + ex.Message);
-                    }
-                }
-
+                SetPlayersPosition(board);
                 var outcome = gameEvaluator.FindGameOutcome(board, Insignia.ToString());
                 _console.WriteLine(outcome.ToString());
                 if (outcome.Status == Status.Won)
